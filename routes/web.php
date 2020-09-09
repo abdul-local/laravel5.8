79a76fd0f0e6635bb4d255db396bda7120f2063c
abdul-local <abdullah.hamzan@gmail.com>
2020-09-10 00:02:29 +0800

    Route Alias Name

--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// menggunakan paramter
-Route::get('/user/{id}/artikel/{idartikel}',function ($id,$idartikel){
-    return 'user' .$id .'artikel' .$idartikel ;
+// buat route
+Route::get('redirect',function(){
+
+    return redirect()->route('homepage');
+
 });
-
-//menggunakan Route Group
-Route::prefix('account')->group(function(){
-    
-    // buat routing Group lagi
-    Route::prefix('setting')->group(function(){
-
-    
-    Route::get('/change-password', function () {
-        return 'change-password';
-    });
-    Route::get('/profile', function () {
-        return 'profile';
-    });
-    Route::get('/artikel', function () {
-        return 'artikel';
-    });
-});
-
-    // buat Routing lagi
-    Route::get('/folower',function(){
-        return 'Folower';
-
-    });
-});
+Route::get('homepage/landing',function(){
+    return 'landing';
+})->name('homepage');
 
